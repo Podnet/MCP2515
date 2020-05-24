19815e16b354c840d68493f8dc737e421fcb1f59
hrithik098 <hrithiky328@gmail.com>
2020-05-24 13:51:46 +0530

    bug fixed

--- src/empty_lib.c
+++
@@ -1,5 +0,0 @@
-#include "mgos.h"
-
-bool mgos_mcp2515_init(void) {
-  return true;
-}
---
+++ src/mgos_mcp2515_c.c
@@ -0,0 +1,9 @@
+ 
+//
+// Created by Bradley Smith on 11/23/17.
+//
+#include <stdbool.h>
+
+bool mgos_MCP2515_init(void) {
+    return true;
+}
